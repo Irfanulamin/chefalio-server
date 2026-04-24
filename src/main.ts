2bc3eb454411122f3312f9782b3a4cab4762f20f
Abdun Nafi Prosun <prosun.swe@gmail.com>
2026-04-25 01:13:38 +0600

    Refactor main.ts to create a NestJS server with Express adapter and dynamic CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
+import express, { Request, Response } from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { rawBody: true });
+const server = express();
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+const createNestServer = async (expressInstance: express.Express) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+    { rawBody: true },
+  );
+
   app.use(cookieParser());
 
   app.useGlobalPipes(
@@ -18,11 +25,50 @@ async function bootstrap() {
     }),
   );
 
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  });
+  app.enableCors(
+    process.env.NODE_ENV === 'production'
+      ? {
+          origin: process.env.ALLOWED_ORIGIN,
+          credentials: true,
+        }
+      : {
+          origin: 'http://localhost:3000',
+          credentials: true,
+        },
+  );
 
-  await app.listen(5000);
+  await app.init();
+};
+
+let isServerReady = false;
+
+export default async function handler(req: Request, res: Response) {
+  if (!isServerReady) {
+    await createNestServer(server);
+    isServerReady = true;
+  }
+  server(req, res);
 }
-bootstrap();
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, { rawBody: true });
+
+//   app.use(cookieParser());
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       forbidNonWhitelisted: true,
+//       transform: true,
+//       forbidUnknownValues: true,
+//     }),
+//   );
+
+//   app.enableCors({
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//   });
+
+//   await app.listen(5000);
+// }
+// bootstrap();
